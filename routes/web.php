9edca74e30666e9bbcea547263e7fce7b4b8304a
jlanir <jacob.lanir@grouphealth.ca>
2018-05-02 16:09:17 -0700

    add is admin check middleware+migration+controller+route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,15 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::name('carving')->post('/carving', 'CarvingController@create')->middleware('auth');;
-Route::get('/carving/{carving}', 'CarvingController@delete')->middleware('auth');
 
-Route::get('/tickets', 'HomeController@showTicket')->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/tickets', 'HomeController@showTicket');
+
+    Route::name('carving')->post('/carving', 'CarvingController@create');
+    Route::get('/carving/{carving}', 'CarvingController@delete');
+});
+
+
+Route::middleware(['auth', \App\Http\Middleware\checkAdmin::class])->group(function () {
+    Route::get('/admin', 'AdminController@viewDashboard');
+});
