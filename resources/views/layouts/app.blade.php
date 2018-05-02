1d65bd1ba587c02f064c3dc81557f71a38557ab0
jlanir <jacob.lanir@grouphealth.ca>
2018-05-02 14:26:50 -0700

    add tickets to system

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
