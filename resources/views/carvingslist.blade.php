107849260194a13514cbbfe0d6420e59cfc181d3
jlanir <jacob.lanir@grouphealth.ca>
2018-05-06 16:37:10 -0700

    move screen to element on new register carcing click

--- resources/views/carvingslist.blade.php
+++ resources/views/carvingslist.blade.php
@@ -1,9 +1,11 @@
 <?php
-    $carvings = \Illuminate\Support\Facades\Auth::user()->carvings->toArray();
+$carvings = \Illuminate\Support\Facades\Auth::user()->carvings->toArray();
 ?>
 
 <div class="card" style="margin-bottom: 30px;">
-    <div class="card-header">{{ __('Registered Carvings') }} <span style="font-weight: bold;float: right;">Total Price: {!! \App\Http\Controllers\HomeController::calcPrice(count($carvings)) !!}$ CAD</span></div>
+    <div class="card-header">{{ __('Registered Carvings') }} <span
+                style="font-weight: bold;float: right;">Total Price: {!! \App\Http\Controllers\HomeController::calcPrice(count($carvings)) !!}
+            $ CAD</span></div>
     <div class="card-body" style="overflow-y: scroll;height: 300px;padding: 0;">
         <table class="table table-striped" style="margin: 0">
             <thead>
@@ -19,24 +21,27 @@
             </thead>
 
             <tbody>
-                @foreach($carvings as $carving)
-                    <tr>
-                        <th scope="row">{!! $carving['id'] !!}</th>
-                        <td>{!! $carving['skill'] !!}</td>
-                        <td>{!! $carving['division'] !!}</td>
-                        <td>{!! $carving['category']!!}</td>
-                        <td>{!! $carving['description'] !!}</td>
-                        <td>{!! $carving['is_for_sale'] !!}</td>
-                        <td>
-                            <button type="button" class="btn btn-info" onclick="window.location = '{{'/carving/'.$carving['id'].'/edit'}}'" style="float: left">Edit</button>
-                            <form action="/carving/{!! $carving['id'] !!}/delete" method="POST" style="float: left">
-                                {{csrf_field()}}
-                                <input type="hidden" value="{{$carving['id']}}" name="id" id="id">
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
+            @foreach($carvings as $carving)
+                <tr>
+                    <th scope="row">{!! $carving['id'] !!}</th>
+                    <td>{!! $carving['skill'] !!}</td>
+                    <td>{!! $carving['division'] !!}</td>
+                    <td>{!! $carving['category']!!}</td>
+                    <td>{!! $carving['description'] !!}</td>
+                    <td>{!! $carving['is_for_sale'] !!}</td>
+                    <td>
+                        <button type="button" class="btn btn-info"
+                                onclick="window.location = '{{'/carving/'.$carving['id'].'/edit'}}'"
+                                style="float: left">Edit
+                        </button>
+                        <form action="/carving/{!! $carving['id'] !!}/delete" method="POST" style="float: left">
+                            {{csrf_field()}}
+                            <input type="hidden" value="{{$carving['id']}}" name="id" id="id">
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
@@ -54,9 +59,14 @@
 
 <script>
     function addCarving() {
-        $('.add-carving').css('display','block');
+        $('.add-carving').css('display', 'block');
         $('#skill').val("");
         $('#division').val("");
         $('#category').val("");
+
+
+        $('html, body').animate({
+            scrollTop: $(".add-carving").offset().top
+        }, 1000);
     }
 </script>
